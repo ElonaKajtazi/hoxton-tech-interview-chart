c1be13c619bf376a9f535b04e44818f1bfb6725b
Elona Kajtazi <MY_NAME@example.com>
2022-10-13 16:55:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+const studentData = [
+  {
+    name: 'Stephen',
+    submissions: {
+      beavers: 3,
+      stars: 2,
+    }
+  },
+  {
+    name: 'Eduardo',
+    submissions: {
+      beavers: 7,
+      stars: 1,
+    }
+  },
+  {
+    name: 'Pepe',
+    submissions: {
+      beavers: 6,
+      stars: 9,
+    }
+  },
+  {
+    name: 'Gigachad',
+    submissions: {
+      beavers: 0,
+      stars: 10,
+    }
+  },
+]
+
+// Using the Recharted library, create a graph as similar as you can, to the one in the #Classroom
+
+function App() {
+  return <>
+  <h1>Hello</h1>
+    {/* Code your solution here */}
+  </>
+
+}
+
+export default App
